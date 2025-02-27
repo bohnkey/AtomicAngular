ba85bca97a855b3f26f8e2a53077c550fc43975c
zachbohn <zach.bohn@keysight.com>
2025-02-26 17:34:10 -0800

    setup capacitor

---
+++ android/app/src/main/java/com/atomicangular/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.atomicangular.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atomicangular.app',
+  appName: 'atomic-angular',
+  webDir: 'dist/atomic-angular/browser'
+};
+
+export default config;
